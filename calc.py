abf6cfea06b05f5fd7aefc3c2f067882c59a85e8
Nijai Giri <nijai.giri@orioninc.com>
2026-02-02 16:23:03 +0530

    calculator prog

---
+++ calc.py
@@ -0,0 +1,24 @@
+print("Simple Calculator")
+print("Operations: +  -  *  /")
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operation (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print("Result:", num1 + num2)
+
+elif op == "-":
+    print("Result:", num1 - num2)
+
+elif op == "*":
+    print("Result:", num1 * num2)
+
+elif op == "/":
+    if num2 == 0:
+        print("Error: Division by zero!")
+    else:
+        print("Result:", num1 / num2)
+
+else:
+    print("Invalid operation!")
